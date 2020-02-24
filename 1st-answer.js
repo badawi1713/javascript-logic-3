ca9021c8575c5e4ca2a5a3ac4007dc3ca02005e6
badawi1713 <dzaky.badawi@gmail.com>
2020-02-24 20:02:46 +0700

    update 1st number

--- 1st-answer.js
+++ 1st-answer.js
@@ -3,28 +3,41 @@ let arraySum = array => {
   for (let i = 0; i < array.length; i++) {
     sum = sum + array[i];
   }
-  let newArray = sum
-    .toString(10)
-    .split("")
-    .map(t => {
-      return parseInt(t);
-    });
-  let sum1 = 0;
-  for (let j = 0; j < newArray.length; j++) {
-    sum1 = sum1 + newArray[j];
+  if (sum == 0) {
+    return console.log("Result:", 0);
+  } else if (sum % 9 == 0) {
+    return console.log("Result:", 9);
+  } else {
+    return console.log("Result:", sum % 9);
   }
-  let newArray1 = sum1
-    .toString(10)
-    .split("")
-    .map(t => {
-      return parseInt(t);
-    });
-  let sum2 = 0;
-  for (let k = 0; k < newArray1.length; k++) {
-    sum2 = sum2 + newArray1[k];
-  }
-  return console.log(`Result: ${sum2}`);
 };
 
 arraySum([123, 9, 27]);
 arraySum([143, 21, 9, 19]);
+
+// let arraySum = array => {
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//   sum = sum + array[i];
+// }
+// let newArray = sum
+//   .toString(10)
+//   .split("")
+//   .map(t => {
+//     return parseInt(t);
+//   });
+// let sum1 = 0;
+// for (let j = 0; j < newArray.length; j++) {
+//   sum1 = sum1 + newArray[j];
+// }
+// let newArray1 = sum1
+//   .toString(10)
+//   .split("")
+//   .map(t => {
+//     return parseInt(t);
+//   });
+// let sum2 = 0;
+// for (let k = 0; k < newArray1.length; k++) {
+//   sum2 = sum2 + newArray1[k];
+// }
+// return console.log(`Result: ${sum2}`);
